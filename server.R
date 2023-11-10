d65c23a13ceefff2c473cd22a0d2b59102c23db4
aswanijehangeer <jahangiraswani237@gmail.com>
2023-11-10 15:17:43 +0500

    server side code

---
+++ server.R
@@ -0,0 +1,75 @@
+
+server <- function(input, output, session) {
+  
+  filtered_data <- reactive({
+    
+    data <- gms_data
+    
+    if (input$federation != "All Federations") {
+      data <- filter(data, federation == input$federation)
+    }
+    if (input$gender != "All") {
+      data <- filter(data, gender == input$gender)
+    }
+    data
+  })
+  # shiny alert ----
+  observe({
+    data <- filtered_data()
+
+    if (nrow(data) == 0) {
+      shinyalert(
+        title = "Data Not Avaiable",
+        text = paste(input$federation, "don't have any", input$gender, "Grandmaster."),
+        size = "m",
+        closeOnEsc = TRUE,
+        closeOnClickOutside = FALSE,
+        html = FALSE,
+        type = "error",
+        showConfirmButton = TRUE,
+        showCancelButton = FALSE,
+        confirmButtonText = "Back",
+        confirmButtonCol = "#FF0400",
+        timer = 0,
+        imageUrl = "",
+        animation = TRUE
+      )
+    }
+  })
+  
+  
+  # map ----
+  output$map <- renderLeaflet({
+    data <- filtered_data()
+    
+    profileIcons <- makeIcon(
+      iconUrl = case_when(
+        data$gender == "Male" ~ "www/Male.png",
+        data$gender == "Female" ~ "www/Female.png"),
+      iconWidth = 20,
+      iconHeight = 20)
+    
+    data |> 
+      leaflet() %>%
+      addTiles() %>%  
+      addMarkers(
+        lng = ~longitude, 
+        lat = ~latitude, 
+        label = ~name,
+        icon = profileIcons,
+        popup = ~popup_text,
+        labelOptions = labelOptions(textsize = "15px")
+      )
+  })
+  # Total Grand masters text ----
+  output$total <- renderText({
+    data <- filtered_data()
+    grandmaster_count <- if (nrow(data) > 0) {
+      nrow(data)
+    } else {
+      0
+    }
+    paste("Total Grandmasters: ", grandmaster_count, sep = "")
+  })
+  
+}
